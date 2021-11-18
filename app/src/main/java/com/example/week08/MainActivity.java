20c9d0e6b05d81fb716c6174a8afbe86b17dc2a6
19120141 <nqt15963@gmail.com>
2021-11-18 15:14:07 +0700

    abc

--- app/src/main/java/com/example/week08/MainActivity.java
+++ app/src/main/java/com/example/week08/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.week08;
 
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 
 import androidx.fragment.app.FragmentActivity;
@@ -13,6 +15,36 @@ public class MainActivity extends FragmentActivity implements com.example.week08
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase db = this.openOrCreateDatabase("myfriendsDB", MODE_PRIVATE, null);
+
+        db.beginTransaction();
+        try {
+            db.execSQL(" DROP TABLE IF EXISTS Lop; ");
+            db.execSQL(" DROP TABLE IF EXISTS HocSinh; ");
+            String sql1 = "create table Lop ("
+                            + "MaLop text PRIMARY KEY,"
+                            + "TenLop text );";
+            String sql2 = "create table HocSinh ("
+                            + "MaHocSinh text PRIMARY KEY,"
+                            + "MaLop text,"
+                            + "TenHocSinh text,"
+                            + "Diem float);";
+            db.execSQL(sql1);
+            db.execSQL(sql2);
+            db.execSQL("insert into Lop(MaLop, TenLop) values('19_3', 'Mobile')");
+            db.execSQL("insert into HocSinh(MaHocSinh, MaLop, TenHocSinh, Diem) values('19120070', '19_3', 'Tran Nhan Hao', 10)");
+            db.execSQL("insert into HocSinh(MaHocSinh, MaLop, TenHocSinh, Diem) values('19120129', '19_3', 'Huynh Minh Thang', 10)");
+            db.execSQL("insert into HocSinh(MaHocSinh, MaLop, TenHocSinh, Diem) values('19120141', '19_3', 'Nguyen Quoc Toan', 10)");
+            db.execSQL("insert into HocSinh(MaHocSinh, MaLop, TenHocSinh, Diem) values('19120168', '19_3', 'Le Viet Bach', 10)");
+            db.execSQL("insert into HocSinh(MaHocSinh, MaLop, TenHocSinh, Diem) values('19120722', '19_3', 'Van The Vinh', 10)");
+
+            db.setTransactionSuccessful(); //commit your changes
+        }
+        catch (SQLiteException e) {
+        //report problem
+        }
+        finally { db.endTransaction(); }
 
         ft = getSupportFragmentManager().beginTransaction();
         fragment1 = com.example.week08.Fragment1.newInstance("first-blue");
